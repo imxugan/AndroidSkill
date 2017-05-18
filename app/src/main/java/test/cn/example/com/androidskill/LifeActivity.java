74061a769182739ebbdc21d8967b0e10cbb31179
imxugan <xugan@xywy.com>
2017-05-18 09:59:26 +0800

    1.在lifeActivity中增加了log日志

--- app/src/main/java/test/cn/example/com/androidskill/LifeActivity.java
+++ app/src/main/java/test/cn/example/com/androidskill/LifeActivity.java
@@ -3,6 +3,8 @@ package test.cn.example.com.androidskill;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import test.cn.example.com.util.LogUtil;
+
 /**
  * activity的生命周期
  */
@@ -12,31 +14,42 @@ public class LifeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life);
-
+        LogUtil.i("onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        LogUtil.i("onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        LogUtil.i("onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        LogUtil.i("onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        LogUtil.i("onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        LogUtil.i("onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        LogUtil.i("onRestart");
     }
 }
