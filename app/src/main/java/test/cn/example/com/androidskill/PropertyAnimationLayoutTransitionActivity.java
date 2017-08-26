c9d408273f05f1b4c68911375a8e6a097b04edc7
xg <imxugan@163.com>
2017-08-26 16:34:01 +0800

    1.给LayoutTransition设置的自定义的动画，未生效

--- app/src/main/java/test/cn/example/com/androidskill/PropertyAnimationLayoutTransitionActivity.java
+++ app/src/main/java/test/cn/example/com/androidskill/PropertyAnimationLayoutTransitionActivity.java
@@ -1,6 +1,8 @@
 package test.cn.example.com.androidskill;
 
+import android.animation.Animator;
 import android.animation.LayoutTransition;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,6 +11,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.GridLayout;
+
+import java.util.Random;
 
 import test.cn.example.com.util.LogUtil;
 
@@ -20,23 +24,38 @@ public class PropertyAnimationLayoutTransitionActivity extends AppCompatActivity
     private ViewGroup ll_contain;
     private int index;
     private GridLayout gridLayout;
+    private LayoutTransition layoutTransition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_property_animation_layout_transition);
         initView();
+//        LayoutTransition 是API Level 11 才出现的。LayoutTransition的动画效果，
+//        只有当ViewGroup中有View添加、删除、隐藏、显示的时候才会体现出来。
+//
+//         LayoutTransition类中主要有五种容器转换动画类型，具体如下：
+//
+//        LayoutTransition.APPEARING            当一个View在ViewGroup中出现时，对此View设置的动画
+//        LayoutTransition.CHANGE_APPEARING     当一个View在ViewGroup中出现时，
+//                          对此View对其他View位置造成影响，对其他View设置的动画
+//        LayoutTransition.DISAPPEARING         当一个View在ViewGroup中消失时，对此View设置的动画
+//        LayoutTransition.CHANGE_DISAPPEARING      当一个View在ViewGroup中消失时，
+//                                      对此View对其他View位置造成影响，对其他View设置的动画
+//        LayoutTransition.CHANGE       不是由于View出现或消失造成对其他View位置造成影响，
+//                                      然后对其他View设置的动画。
+//        注意动画到底设置在谁身上，此View还是其他View。
     }
 
     private void initView() {
         Button addBtn = (Button) findViewById(R.id.addBtn);
         addBtn.setOnClickListener(this);
-        CheckBox appearance = (CheckBox) findViewById(R.id.appearance);
+        CheckBox appearance = (CheckBox) findViewById(R.id.appearing);
         appearance.setOnCheckedChangeListener(this);
-        CheckBox change_appearance = (CheckBox) findViewById(R.id.change_appearance);
+        CheckBox change_appearance = (CheckBox) findViewById(R.id.change_appearing);
         change_appearance.setOnCheckedChangeListener(this);
-        CheckBox disappear = (CheckBox) findViewById(R.id.disappear);
+        CheckBox disappear = (CheckBox) findViewById(R.id.disappearing);
         disappear.setOnCheckedChangeListener(this);
-        CheckBox change_disappear = (CheckBox) findViewById(R.id.change_disappear);
+        CheckBox change_disappear = (CheckBox) findViewById(R.id.change_disappearing);
         change_disappear.setOnCheckedChangeListener(this);
 
         ll_contain = (ViewGroup) findViewById(R.id.ll_contain);
@@ -44,7 +63,7 @@ public class PropertyAnimationLayoutTransitionActivity extends AppCompatActivity
         gridLayout = new GridLayout(this);
         gridLayout.setColumnCount(5);
         ll_contain.addView(gridLayout);
-        LayoutTransition layoutTransition = new LayoutTransition();
+        layoutTransition = new LayoutTransition();
         gridLayout.setLayoutTransition(layoutTransition);
     }
 
@@ -54,33 +73,47 @@ public class PropertyAnimationLayoutTransitionActivity extends AppCompatActivity
             case R.id.addBtn:
                 addBtn();
                 break;
-            case R.id.change_appearance:
-                change_appearance();
-                break;
-            case R.id.disappear:
-                disappear();
-                break;
-            case R.id.change_disappear:
-                change_disappear();
-                break;
         }
     }
 
-    private void change_disappear(){
+    private void change_disappearing(boolean isChecked) {
+        layoutTransition = new LayoutTransition();
+        layoutTransition.setAnimator(LayoutTransition.CHANGE_DISAPPEARING,
+                isChecked?layoutTransition.getAnimator(LayoutTransition.CHANGE_DISAPPEARING):null);
+    }
+
+    private void disappearing(boolean isChecked) {
+        layoutTransition = new LayoutTransition();
+        layoutTransition.setAnimator(LayoutTransition.DISAPPEARING,
+                isChecked?layoutTransition.getAnimator(LayoutTransition.DISAPPEARING):null);
+    }
+
+    private void change_appearing(PropertyAnimationLayoutTransitionActivity button ,boolean isChecked){
+        layoutTransition = new LayoutTransition();
+        layoutTransition.setAnimator(LayoutTransition.CHANGE_APPEARING,
+               isChecked?ObjectAnimator.ofFloat(button,"scaleX",1f,1.5f):null);
 
     }
 
-    private void disappear(){
+    private void appearing(boolean isChecked) {
+        layoutTransition = new LayoutTransition();
+//        layoutTransition.setAnimator(LayoutTransition.APPEARING,
+//                isChecked?layoutTransition.getAnimator(LayoutTransition.APPEARING):null);
 
-    }
-
-    private void change_appearance(){
-
+        LogUtil.i("isChecked========"+isChecked);
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(this, "rotationY", 0F, 90F, 0F);
+        layoutTransition.setAnimator(LayoutTransition.APPEARING, isChecked?animator1:null);
     }
 
     private void addBtn(){
-        final Button button = new Button(this);
-        button.setText(index++);
+        final Button button = new Button(PropertyAnimationLayoutTransitionActivity.this);
+        button.setText(""+(index++));
+//        int childCount = gridLayout.getChildCount();
+//        if(childCount>3){
+//            gridLayout.addView(button, new Random().nextInt(childCount));
+//        }else {
+//            gridLayout.addView(button);
+//        }
         gridLayout.addView(button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,18 +121,29 @@ public class PropertyAnimationLayoutTransitionActivity extends AppCompatActivity
                 gridLayout.removeView(button);
             }
         });
+        LogUtil.i("?=="+layoutTransition.getAnimator(LayoutTransition.APPEARING));
+        gridLayout.setLayoutTransition(layoutTransition);
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         switch (buttonView.getId()){
-            case R.id.appearance:
-                LogUtil.i("appearance");
+            case R.id.appearing:
+                appearing(isChecked);
                 break;
-            case R.id.change_appearance:
-                LogUtil.i("change_appearance");
+            case R.id.change_appearing:
+                LogUtil.i("this="+this);
+                change_appearing(this,isChecked);
+                break;
+            case R.id.disappearing:
+                disappearing(isChecked);
+                break;
+            case R.id.change_disappearing:
+                change_disappearing(isChecked);
                 break;
 
         }
     }
+
+
 }
