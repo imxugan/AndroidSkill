af369fc248a5a2f832e1074e298c96c57320b0a3
imxugan <xugan@xywy.com>
2017-09-25 16:53:49 +0800

    1.添加了ArcMenuViewGroup控件，但是还未实现动画效果。

--- app/src/main/java/test/cn/example/com/androidskill/ArcMenuActivity.java
+++ app/src/main/java/test/cn/example/com/androidskill/ArcMenuActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import test.cn.example.com.util.LogUtil;
+
 /**
  * Created by xgxg on 2017/9/25.
  */
@@ -13,5 +15,6 @@ public class ArcMenuActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arc_menu);
+        LogUtil.i("guoguo");
     }
 }
