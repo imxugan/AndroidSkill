0e552d5ab5ff29cd12b5be5520b370b7e3613daa
xg <imxugan@163.com>
2018-12-11 23:10:59 +0800

    1.在hencoder的添加了viewPropertyAnimation的PathInterpolator的动画演示,但是未出效果

--- app/src/main/java/test/cn/example/com/androidskill/view/defineView/hencoder/practice_six/HencoderPracticeSixFragment.java
+++ app/src/main/java/test/cn/example/com/androidskill/view/defineView/hencoder/practice_six/HencoderPracticeSixFragment.java
@@ -2,6 +2,7 @@ package test.cn.example.com.androidskill.view.defineView.hencoder.practice_six;
 
 import android.animation.Animator;
 import android.animation.TimeInterpolator;
+import android.graphics.Path;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -20,6 +21,7 @@ import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.OvershootInterpolator;
+import android.view.animation.PathInterpolator;
 import android.widget.LinearLayout;
 
 import test.cn.example.com.androidskill.R;
@@ -30,6 +32,19 @@ import test.cn.example.com.androidskill.view.defineView.hencoder.practice_four.C
 import test.cn.example.com.util.LogUtil;
 
 /**
+ * 1：AccelerateDecelerateInterpolator 加速减速插补器（先慢后快再慢）
+ 2：AccelerateInterpolator 加速插补器（先慢后快）
+ 3：AnticipateInterpolator 向前插补器（先往回跑一点，再加速向前跑）
+ 4：AnticipateOvershootInterpolator 向前向后插补器（先往回跑一点，再向后跑一点，再回到终点）
+ 5：BounceInterpolator 反弹插补器（在动画结束的时候回弹几下，如果是竖直向下运动的话，就是玻璃球下掉弹几下的效果）
+ 6：CycleInterpolator 循环插补器（按指定的路径以指定时间（或者是偏移量）的1/4、变速地执行一遍，再按指定的轨迹的相反反向走1/2的时间，再按指定的路径方向走完剩余的1/4的时间，最后回到原点。假如：默认是让a从原点往东跑100米。它会先往东跑100米，然后往西跑200米，再往东跑100米回到原点。可在代码中指定循环的次数）
+ 7：DecelerateInterpolator 减速插补器（先快后慢）
+ 8：LinearInterpolator 直线插补器（匀速）
+ 9：OvershootInterpolator 超出插补器（向前跑直到越界一点后，再往回跑）
+ 10：FastOutLinearInInterpolator MaterialDesign基于贝塞尔曲线的插补器 效果：依次 慢慢快
+ 11：FastOutSlowInInterpolator MaterialDesign基于贝塞尔曲线的插补器 效果：依次 慢快慢
+ 12：LinearOutSlowInInterpolator MaterialDesign基于贝塞尔曲线的插补器 效果：依次 快慢慢
+
  * Created by xugan on 2018/11/12.
  */
 
@@ -67,6 +82,7 @@ public class HencoderPracticeSixFragment extends Fragment implements View.OnClic
                 ll.findViewById(R.id.btn_AnticipateOvershootInterpolator).setOnClickListener(this);
                 ll.findViewById(R.id.btn_OvershootInterpolator).setOnClickListener(this);
                 ll.findViewById(R.id.btn_Interpolator).setOnClickListener(this);
+                ll.findViewById(R.id.btn_PathInterpolator).setOnClickListener(this);
                 root.addView(ll);
                 break;
             case 1:
@@ -208,52 +224,65 @@ public class HencoderPracticeSixFragment extends Fragment implements View.OnClic
                 }
                 break;
             case R.id.btn_LinearInterpolator:
+                //LinearInterpolator  动画匀速改变
                 viewPropertyAnimator.setInterpolator(new LinearInterpolator());
                 viewPropertyAnimator.translationX(300);
                 break;
             case R.id.btn_AccelerateInterpolator:
+                //AccelerateInterpolator  加速插补器（先慢后快）
                 viewPropertyAnimator.setInterpolator(new LinearInterpolator());
                 viewPropertyAnimator.translationX(300);
                 viewPropertyAnimator2.setInterpolator(new AccelerateInterpolator(10));
                 viewPropertyAnimator2.translationX(300);
                 break;
             case R.id.btn_AccelerateDecelerateInterpolator:
+                //AccelerateDecelerateInterpolator   动画始末速率较慢，中间加速
                 viewPropertyAnimator.setInterpolator(new LinearInterpolator());
                 viewPropertyAnimator.translationX(300);
                 viewPropertyAnimator2.setInterpolator(new AccelerateDecelerateInterpolator());
                 viewPropertyAnimator2.translationX(300);
                 break;
             case R.id.btn_DecelerateInterpolator:
+                //DecelerateInterpolator 减速插补器（先快后慢）
                 viewPropertyAnimator.setInterpolator(new LinearInterpolator());
                 viewPropertyAnimator.translationX(300);
                 viewPropertyAnimator2.setInterpolator(new DecelerateInterpolator(10f));
                 viewPropertyAnimator2.translationX(300);
                 break;
             case R.id.btn_BounceInterpolator:
+                //BounceInterpolator  反弹插补器（在动画结束的时候回弹几下，如果是竖直向下运动的话，就是玻璃球下掉弹几下的效果）
                 viewPropertyAnimator.setInterpolator(new LinearInterpolator());
                 viewPropertyAnimator.translationX(300);
                 viewPropertyAnimator2.setInterpolator(new BounceInterpolator());
                 viewPropertyAnimator2.translationX(300);
                 break;
             case R.id.btn_CycleInterpolator:
+                //CycleInterpolator   循环播放速率改变为正弦曲线
+                //CycleInterpolator  循环插补器（按指定的路径以指定时间（或者是偏移量）的1/4、变速地执行一遍，
+                // 再按指定的轨迹的相反反向走1/2的时间，再按指定的路径方向走完剩余的1/4的时间，最后回到原点。
+                // 假如：默认是让a从原点往东跑100米。它会先往东跑100米，然后往西跑200米，再往东跑100米回到原点。
+                // 可在代码中指定循环的次数）
                 viewPropertyAnimator.setInterpolator(new LinearInterpolator());
                 viewPropertyAnimator.translationX(300);
                 viewPropertyAnimator2.setInterpolator(new CycleInterpolator(2));
                 viewPropertyAnimator2.translationX(300);
                 break;
             case R.id.btn_AnticipateInterpolator:
+                //AnticipateInterpolator  向前插补器（先往回跑一点，再加速向前跑）
                 viewPropertyAnimator.setInterpolator(new LinearInterpolator());
                 viewPropertyAnimator.translationX(300);
                 viewPropertyAnimator2.setInterpolator(new AnticipateInterpolator(10));
                 viewPropertyAnimator2.translationX(300);
                 break;
             case R.id.btn_AnticipateOvershootInterpolator:
+                //AnticipateOvershootInterpolator  向前向后插补器（先往回跑一点，再向后跑一点，再回到终点）
                 viewPropertyAnimator.setInterpolator(new LinearInterpolator());
                 viewPropertyAnimator.translationX(300);
                 viewPropertyAnimator2.setInterpolator(new AnticipateOvershootInterpolator(10));
                 viewPropertyAnimator2.translationX(300);
                 break;
             case R.id.btn_OvershootInterpolator:
+                //OvershootInterpolator  超出插补器（向前跑直到越界一点后，再往回跑）
                 viewPropertyAnimator.setInterpolator(new LinearInterpolator());
                 viewPropertyAnimator.translationX(300);
                 viewPropertyAnimator2.setInterpolator(new OvershootInterpolator(10));
@@ -270,6 +299,17 @@ public class HencoderPracticeSixFragment extends Fragment implements View.OnClic
                     }
                 });
                 viewPropertyAnimator2.translationX(300);
+                break;
+            case R.id.btn_PathInterpolator:
+                //PathInterpolator  新增，定义路径坐标后按照路径坐标来跑
+                viewPropertyAnimator.setInterpolator(new LinearInterpolator());
+                viewPropertyAnimator.translationX(300);
+                Path path = new Path();
+                path.cubicTo(0.2f,0f,0.1f,1f,0.5f,1f);
+                path.lineTo(1f,1f);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    viewPropertyAnimator2.setInterpolator(new PathInterpolator(path));
+                }
                 break;
         }
     }
