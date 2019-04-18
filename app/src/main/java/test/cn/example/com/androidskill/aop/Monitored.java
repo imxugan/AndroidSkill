13b724a31dc4b9d8558be859d37f3b8c97b3befb
xg <imxugan@163.com>
2019-04-18 23:00:03 +0800

    1.在app这个module中，集成了aspectJ

---
+++ app/src/main/java/test/cn/example/com/androidskill/aop/Monitored.java
@@ -0,0 +1,15 @@
+package test.cn.example.com.androidskill.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by xugan on 2019/4/18.
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Monitored {
+}
