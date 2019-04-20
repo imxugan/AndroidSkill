83b3b9bf865919d66b9e5b8945cef17d74aceda4
xg <imxugan@163.com>
2019-04-20 17:55:57 +0800

    1.在app这个module中，调整了LiveDataBus类的功能，保证了，在其他还未启动的类虽然订阅了消息，但是，启动后，还能接受到消息问题

--- app/src/main/java/test/cn/example/com/androidskill/livedatabus/LiveDataBus.java
+++ app/src/main/java/test/cn/example/com/androidskill/livedatabus/LiveDataBus.java
@@ -1,7 +1,13 @@
 package test.cn.example.com.androidskill.livedatabus;
 
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
+import android.support.annotation.NonNull;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,10 +30,62 @@ public class LiveDataBus {
     }
 
     //提供外部调用的方法
-    public synchronized <T> MutableLiveData<T> with(String key,Class<T> type){
+    public synchronized <T> BusMutableLiveData<T> with(String key,Class<T> type){
         if(!map.containsKey(key)){
-            map.put(key,new MutableLiveData<Object>());
+            map.put(key,new BusMutableLiveData<Object>());
         }
-        return (MutableLiveData<T>) map.get(key);
+        return (BusMutableLiveData<T>) map.get(key);
+    }
+
+    /**
+     * 重写MutableLiveData 在observe方法中进行hook
+     */
+    public static class BusMutableLiveData<T> extends MutableLiveData<T>{
+        @Override
+        public void observe(@NonNull LifecycleOwner owner, @NonNull Observer<T> observer) {
+            super.observe(owner, observer);
+            try {
+                hook(observer);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void hook(Observer<T> observer) throws Exception {
+            //获取liveData类的class对象
+            Class<LiveData> liveDataClazz = LiveData.class;
+            //通过反射获取LiveData里面的Observer
+            Field mObservers = liveDataClazz.getDeclaredField("mObservers");
+            //设置成员变量可以访问
+            mObservers.setAccessible(true);
+            //获取这个成员变量的值，这个值是一个map
+            Object objectObservers = mObservers.get(this);
+            //获取这个map的class对象
+            Class<?> observerClazz = objectObservers.getClass();
+            //获取到observerClazz里面的get方法
+            Method get = observerClazz.getDeclaredMethod("get", Object.class);
+            //设置这个get方法能够暴力访问
+            get.setAccessible(true);
+            Object invokeEntry = get.invoke(objectObservers, observer);
+            //定一个空的对象
+            Object objectWrapper = null;
+            if(invokeEntry instanceof Map.Entry){
+                objectWrapper = ((Map.Entry)invokeEntry).getValue();
+            }
+            if(null == objectWrapper){
+                throw new NullPointerException("Wrapper can not be null!'");
+            }
+            Class<?> superclass = objectWrapper.getClass().getSuperclass();
+            //通过superclass获取到mLastVersion成员变量
+            Field mLastVersionField = superclass.getDeclaredField("mLastVersion");
+            //设置mLastVersion变量暴力访问
+            mLastVersionField.setAccessible(true);
+            Field mVersionFile = liveDataClazz.getDeclaredField("mVersion");
+            mVersionFile.setAccessible(true);
+            //获取mVerson的值
+            Object mVersonValue = mVersionFile.get(this);
+            //将mVerson成员变量的值赋值给mLastVersion字段
+            mLastVersionField.set(objectWrapper,mVersonValue);
+        }
     }
 }
