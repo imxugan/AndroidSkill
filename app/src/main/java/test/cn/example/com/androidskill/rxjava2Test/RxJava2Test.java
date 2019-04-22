952fa7ef2382790fa7aa982889400c22ad563e75
xg <imxugan@163.com>
2019-04-22 15:47:52 +0800

    1.在app这个module中，添加了rxjava2包，在这个包中，添加了rxjava2的线程控制的使用演示

--- app/src/main/java/test/cn/example/com/androidskill/rxjava2Test/RxJava2Test.java
+++ app/src/main/java/test/cn/example/com/androidskill/rxjava2Test/RxJava2Test.java
@@ -6,6 +6,7 @@ import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by xugan on 2019/4/22.
@@ -13,6 +14,41 @@ import io.reactivex.disposables.Disposable;
 
 public class RxJava2Test {
     public static void main(String[] args){
+        test();
+        test2();
+    }
+
+    private static void test2() {
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("abc");
+                emitter.onComplete();
+            }
+        }).subscribeOn(Schedulers.io()).subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                System.out.println("onSubscribe    d.isDisposed()="+d.isDisposed()+"     "+Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                System.out.println(s+"     "+Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                System.out.println(e.getMessage()+"     "+Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete      "+ Thread.currentThread().getName());
+            }
+        });
+    }
+
+    private static void test() {
         Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Exception {
@@ -22,24 +58,27 @@ public class RxJava2Test {
             }
         });
         Observer<Integer> observer = new Observer<Integer>() {
+            private Disposable disposable;
             @Override
             public void onSubscribe(@NonNull Disposable d) {
-                System.out.println("onSubscribe");
+                this.disposable = d;
+                System.out.println("onSubscribe     "+Thread.currentThread().getName());
             }
 
             @Override
             public void onNext(@NonNull Integer s) {
-               System.out.println(s+"");
+                //isDisposed()方法，是否取消了订阅关系
+                System.out.println(s+"      "+disposable.isDisposed()+"     "+Thread.currentThread().getName());
             }
 
             @Override
             public void onError(@NonNull Throwable e) {
-                System.out.println("onError");
+                System.out.println("onError"+"      "+Thread.currentThread().getName());
             }
 
             @Override
             public void onComplete() {
-                System.out.println("onComplete");
+                System.out.println("onComplete"+"       "+Thread.currentThread().getName());
             }
         };
         observable.subscribe(observer);
