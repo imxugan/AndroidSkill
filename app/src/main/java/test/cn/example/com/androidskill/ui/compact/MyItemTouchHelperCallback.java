52dde502b986459637a91a9e0fd8059d0ad50fbf
xg <imxugan@163.com>
2019-06-19 00:13:53 +0800

    在app这个module中，添加了ui模块，并在ui模块中增加了ItemTouchHelper的使用，完成了左右侧滑条目时增加了透明度改变，缩放的改变的动画效果

--- app/src/main/java/test/cn/example/com/androidskill/ui/compact/MyItemTouchHelperCallback.java
+++ app/src/main/java/test/cn/example/com/androidskill/ui/compact/MyItemTouchHelperCallback.java
@@ -1,6 +1,8 @@
 package test.cn.example.com.androidskill.ui.compact;
 
+import android.graphics.Canvas;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
@@ -47,6 +49,32 @@ public class MyItemTouchHelperCallback extends ItemTouchHelper.Callback{
             //向左或者向右侧滑时
             mItemMoveListener.onItemRemove(viewHolder.getAdapterPosition());
         }
+    }
 
+    @Override
+    public void onSelectedChanged(@Nullable RecyclerView.ViewHolder viewHolder, int actionState) {
+        super.onSelectedChanged(viewHolder, actionState);
+        //选择的条目，改变背景颜色
+        if(ItemTouchHelper.ACTION_STATE_IDLE != actionState){
+            viewHolder.itemView.setBackgroundColor(viewHolder.itemView.getContext().getResources().getColor(android.R.color.holo_blue_light));
+        }
+    }
+
+    @Override
+    public void clearView(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        super.clearView(recyclerView, viewHolder);
+        //恢复选中的条目的背景颜色
+        viewHolder.itemView.setBackgroundColor(viewHolder.itemView.getContext().getResources().getColor(android.R.color.transparent));
+    }
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        //dX 横向移动的增量
+        //dY 纵向移动的增量
+        float alpha = 1 - (Math.abs(dX)) / viewHolder.itemView.getWidth();
+        viewHolder.itemView.setAlpha(alpha);
+        viewHolder.itemView.setScaleX(alpha);
+        viewHolder.itemView.setScaleY(alpha);
     }
 }
