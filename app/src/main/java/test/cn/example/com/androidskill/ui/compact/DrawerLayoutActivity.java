a699e2c84b5298f995b2a3c230d9949d0e275e9b
xg <imxugan@163.com>
2019-06-19 23:05:49 +0800

    在app这个module中的ui模块中，添加了DrawerLayout的基本使用演示，在toolbar中添加了ActionBarDrawerToggle的效果

--- app/src/main/java/test/cn/example/com/androidskill/ui/compact/DrawerLayoutActivity.java
+++ app/src/main/java/test/cn/example/com/androidskill/ui/compact/DrawerLayoutActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import test.cn.example.com.androidskill.R;
 
@@ -21,6 +20,9 @@ public class DrawerLayoutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drawerlayout);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);//用toolbar替代actionbar
-
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
+        drawerToggle.syncState();//同步状态
+        drawerLayout.setDrawerListener(drawerToggle);
     }
 }
