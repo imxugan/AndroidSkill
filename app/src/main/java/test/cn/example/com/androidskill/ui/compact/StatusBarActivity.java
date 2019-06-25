97af0dd7d2f1b97a96a781c2baa4349f0e37f137
xg <imxugan@163.com>
2019-06-25 23:55:21 +0800

    1.在app这个module中的ui模块中，添加了沉浸式状态栏的部分演示,目前只兼容到了5.x版本以上，5.x以下，还未兼容

--- app/src/main/java/test/cn/example/com/androidskill/ui/compact/StatusBarActivity.java
+++ app/src/main/java/test/cn/example/com/androidskill/ui/compact/StatusBarActivity.java
@@ -1,5 +1,6 @@
 package test.cn.example.com.androidskill.ui.compact;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -15,5 +16,8 @@ public class StatusBarActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statusbar);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.black_deep));
+        }
     }
 }
