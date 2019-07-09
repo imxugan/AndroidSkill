449b5a3616b94fcbc8c4e7f2ef504acd79c2601e
xg <imxugan@163.com>
2019-07-09 20:15:12 +0800

    1.在app这个module中，增加了kotlin 的常量与变量的使用演示

--- app/src/main/java/test/cn/example/com/androidskill/kotlin/chapter2/KotlinChapter2Demo1.kt
+++ app/src/main/java/test/cn/example/com/androidskill/kotlin/chapter2/KotlinChapter2Demo1.kt
@@ -19,8 +19,32 @@ class KotlinChapter2Demo1 : AppCompatActivity() {
 
         LogUtil.i(""+THOUSAND+"         "+aInt+"             "+bInt)
 
+        //无返回值的函数
+        sayHi("jack ma",40)
 
+        var sum = add(3,5)
+        LogUtil.i(""+sum)
 
+        var result = mulipe(23,2)
+        LogUtil.i(""+result)
 
+        var intResult = int2Long(34)
+        LogUtil.i(""+intResult)
+    }
+
+    fun sayHi(name:String,age:Int):Unit{
+        LogUtil.i(name+"            "+age)
+    }
+
+    fun add(a:Int,b:Int):Int{
+        return a+b
+    }
+
+    //函数体中只是表达式，可以这样简写
+    fun mulipe(a:Int,b:Int) = a*b
+
+    //匿名函数，但是要用一个变量接收，否则，这个匿名函数无法被调用
+    var int2Long = fun(a:Int):Long{
+        return a.toLong()
     }
 }
