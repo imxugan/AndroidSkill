d4dcc474e92314a0ca13f59d78ec641e657327a7
xg <imxugan@163.com>
2019-07-16 21:45:59 +0800

    1.在app这个module中，增加了kotlin的 偏函数 使用演示

--- app/src/main/java/test/cn/example/com/androidskill/kotlin/chapter4/KotlinChapter4Demo6.kt
+++ app/src/main/java/test/cn/example/com/androidskill/kotlin/chapter4/KotlinChapter4Demo6.kt
@@ -3,7 +3,9 @@ package test.cn.example.com.androidskill.kotlin.chapter4
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import test.cn.example.com.androidskill.R
+import test.cn.example.com.util.LogUtil
 import java.io.OutputStream
+import java.nio.charset.Charset
 
 class KotlinChapter4Demo6 : AppCompatActivity() {
 
@@ -16,7 +18,37 @@ class KotlinChapter4Demo6 : AppCompatActivity() {
         //调用科里化的写法的函数
         log2("MY_LOG")(System.out)("999999")
         ::log.curried()("MY_LOG")(System.out)("999999")
+
+        //只是传入部分参数，得到了一个新的函数吗，这个函数就称为偏函数
+        val partial = log2("MY_LOG")(System.out)
+        partial("偏函数的输出结果")
+
+        val result = makeStringPartical2("helloworkd".toByteArray(charset("GBK")))
+        LogUtil.i(result)
+
+        //这里估计会输出乱码，因为，第一个参数固定是utf-8对传入的bytearray进行解码，但是传入的字符是进行gbk编码的
+        val result2 = makeStringPartical1(charset("GBK"))
+        LogUtil.i(result2)
     }
+
+    val makeString = fun(byteArray:ByteArray,charset:Charset):String{
+        return String(byteArray,charset)
+    }
+
+
+
+    //实现固定第二个参数的偏函数
+    fun <P1,P2,R> Function2<P1,P2,R>.partical2(p2:P2)=fun(p1:P1)=this(p1,p2)
+    //创建第二个参数是GBK的偏函数
+    val makeStringPartical2 = makeString.partical2(charset("GBK"))
+
+    //创建一个第一个参数默认是 test partical 默认是utf-8编码的    的偏函数
+    val makeStringPartical1 = makeString.partical1("test partical 默认是utf-8编码的".toByteArray(charset("utf-8")))
+
+    //实现固定第一个参数的偏函数
+    fun <P1,P2,R> Function2<P1,P2,R>.partical1(p1:P1)=fun(p2:P2)=this(p1,p2)
+
+
 
     fun log(tag:String,target:OutputStream,messaeg:Any?){
         target.write("$tag  $messaeg\n".toByteArray())
