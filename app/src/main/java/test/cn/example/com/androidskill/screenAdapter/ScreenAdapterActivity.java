b2bcd479605b19405932fef1e97de9e0b07bb5ea
xugan <xugan@xywy.com>
2019-08-22 16:14:05 +0800

    1.在app这个module中,调整了ScreenAdapterActivity继承的父类

--- app/src/main/java/test/cn/example/com/androidskill/screenAdapter/ScreenAdapterActivity.java
+++ app/src/main/java/test/cn/example/com/androidskill/screenAdapter/ScreenAdapterActivity.java
@@ -1,30 +1,19 @@
 package test.cn.example.com.androidskill.screenAdapter;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 
 import test.cn.example.com.androidskill.R;
-import test.cn.example.com.androidskill.base.BaseActivity;
 import test.cn.example.com.androidskill.util.DensityUtil;
 import test.cn.example.com.util.LogUtil;
 
-public class ScreenAdapterActivity extends BaseActivity {
+public class ScreenAdapterActivity extends AppCompatActivity {
     @Override
-    public int getLayoutId() {
-        return R.layout.activity_screen_adapter;
-    }
-
-    @Override
-    public int getMyDefinedEmptyLayoutId() {
-        return 0;
-    }
-
-    @Override
-    public int getMyDefinedErrorLayoutId() {
-        return 0;
-    }
-
-    @Override
-    public void initView() {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_adapter);
         DisplayMetrics metrics = DensityUtil.getDisplayMetrics(this);
         LogUtil.i("density= "+metrics.density);
         LogUtil.i("densityDpi= "+metrics.densityDpi);
