f4a4c1871bcc384beb93090abc8023ab8cf55fbc
xugan <xugan@xywy.com>
2019-08-22 22:00:36 +0800

    1.在app这个module中,新增了drawable开头的各个文件夹，并将相应的mipmap中的test_meitu.png移动到了新建的drawable文件夹中

--- app/src/main/java/test/cn/example/com/androidskill/screenAdapter/ScreenAdapterActivity.java
+++ app/src/main/java/test/cn/example/com/androidskill/screenAdapter/ScreenAdapterActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ImageView;
 
 import test.cn.example.com.androidskill.R;
 import test.cn.example.com.androidskill.util.DensityUtil;
@@ -18,5 +20,24 @@ public class ScreenAdapterActivity extends AppCompatActivity {
         LogUtil.i("density= "+metrics.density);
         LogUtil.i("densityDpi= "+metrics.densityDpi);
         LogUtil.i(metrics.widthPixels+"         "+metrics.heightPixels);
+        final ImageView iv_1 = findViewById(R.id.iv_1);
+        final ImageView iv_2 = findViewById(R.id.iv_2);
+        iv_1.post(new Runnable() {
+            @Override
+            public void run() {
+                int width = iv_1.getWidth();
+                int height = iv_1.getHeight();
+                LogUtil.i("iv_1     "+width+"       "+height);
+            }
+        });
+        iv_2.post(new Runnable() {
+            @Override
+            public void run() {
+                int width = iv_2.getWidth();
+                int height = iv_2.getHeight();
+                LogUtil.i("iv_2     "+width+"       "+height);
+            }
+        });
+
     }
 }
