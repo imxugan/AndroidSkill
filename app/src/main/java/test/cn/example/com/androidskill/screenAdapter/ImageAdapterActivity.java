22c9f83511acf46f60c940024d9e6801dc2dc6d1
xugan <xugan@xywy.com>
2019-08-23 16:41:52 +0800

    1.在app这个module中,调整了屏幕适配这个目录的结构

---
+++ app/src/main/java/test/cn/example/com/androidskill/screenAdapter/ImageAdapterActivity.java
@@ -0,0 +1,56 @@
+package test.cn.example.com.androidskill.screenAdapter;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
+
+import test.cn.example.com.androidskill.R;
+import test.cn.example.com.androidskill.util.DensityUtil;
+import test.cn.example.com.util.LogUtil;
+
+public class ImageAdapterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_adapter);
+        DisplayMetrics metrics = DensityUtil.getDisplayMetrics(this);
+        LogUtil.i(metrics.toString());
+        LogUtil.i("density= "+metrics.density);
+        LogUtil.i("densityDpi= "+metrics.densityDpi);
+        LogUtil.i(metrics.widthPixels+"         "+metrics.heightPixels);
+        final ImageView iv_1 = findViewById(R.id.iv_1);
+        final ImageView iv_2 = findViewById(R.id.iv_2);
+        printBitmapSize(iv_1);
+        iv_1.post(new Runnable() {
+            @Override
+            public void run() {
+                int width = iv_1.getWidth();
+                int height = iv_1.getHeight();
+                LogUtil.i("iv_1     "+width+"       "+height);
+            }
+        });
+        iv_2.post(new Runnable() {
+            @Override
+            public void run() {
+                int width = iv_2.getWidth();
+                int height = iv_2.getHeight();
+                LogUtil.i("iv_2     "+width+"       "+height);
+            }
+        });
+
+    }
+
+    private void printBitmapSize(ImageView imageView){
+        Drawable drawable = imageView.getDrawable();
+        if(null != drawable){
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            Bitmap bitmap = bitmapDrawable.getBitmap();
+            LogUtil.i("图片大小  "+bitmap.getByteCount());
+        }
+    }
+}
