9cb3086f64aa50d707c3e52574363ec33fa5fdbe
xugan <xugan@xywy.com>
2019-09-03 15:17:55 +0800

    1.在CustomProgressBar3这个自定义view中，增加了处理了padding的相关逻辑

--- app/src/main/java/test/cn/example/com/androidskill/ui/compact/CustomProgerssBar3.java
+++ app/src/main/java/test/cn/example/com/androidskill/ui/compact/CustomProgerssBar3.java
@@ -32,10 +32,14 @@ public class CustomProgerssBar3 extends View {
     private final int FILL = 1;
     private final int style;
     private final int max;
+    private final int paddingRight;
+    private final int paddingBottom;
     private int progress;
     private final Paint mPaint;
     private int defaultWidth = 0;
     private int defaultHeight = 0;
+    private final int paddingLeft;
+    private final int paddingTop;
 
     public CustomProgerssBar3(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -53,6 +57,11 @@ public class CustomProgerssBar3 extends View {
         typedArray.recycle();
         defaultWidth = DensityUtil.dip2px(context,60);
         defaultHeight = DensityUtil.dip2px(context,60);
+        paddingLeft = getPaddingLeft();
+        paddingRight = getPaddingRight();
+        paddingTop = getPaddingTop();
+        paddingBottom = getPaddingBottom();
+        LogUtil.i("paddingLeft= "+ paddingLeft+"  paddingRight="+paddingRight+"  paddingTop="+paddingTop+"  paddingBottom="+paddingBottom);
 
     }
 
@@ -70,8 +79,13 @@ public class CustomProgerssBar3 extends View {
         mPaint.setStyle((style==STROKE)? Paint.Style.STROKE: Paint.Style.FILL);
         mPaint.setStrokeWidth(roundWidth);
         mPaint.setColor(roundProgressColor);
-        int center = getWidth()/2;
-        canvas.drawCircle(center,center,center-roundWidth/2,mPaint);
+        int width = getWidth()-paddingLeft-paddingRight;
+        int height = getHeight()-paddingTop-paddingBottom;
+        int centerX = paddingLeft+width/2;
+        int centerY = paddingTop+height/2;
+        int radius = Math.min(width,height)/2;
+        radius = (int) (radius-roundWidth/2);
+        canvas.drawCircle(centerX,centerY,radius,mPaint);
         float percent = 0.0f;
         if(textShow){
             //绘制进度百分比
@@ -88,11 +102,11 @@ public class CustomProgerssBar3 extends View {
             int textWidth = bounds.width();
 //        measureText() 会在左右两侧加上一些额外的宽度值，而 getTextBounds() 则是返回需要的最小宽度而已
             LogUtil.i("textWidth="+textWidth+"        textHeight="+textHeight+"       mPaint.measureText(text)="+mPaint.measureText(text));
-            float x = (getWidth() - textWidth) / 2;
+            float x =  centerX-textWidth/2;
             Paint.FontMetrics fontMetrics = mPaint.getFontMetrics();
             LogUtil.e("fontMetrics.descent="+fontMetrics.descent+"          fontMetrics.ascent="+fontMetrics.ascent);
             LogUtil.i("descent="+mPaint.descent()+"         ascent="+mPaint.ascent()+"     textHeight="+(mPaint.descent()-mPaint.ascent()));
-            canvas.drawText(text, x,(center+textHeight/2),mPaint);
+            canvas.drawText(text, x,(centerY+textHeight/2),mPaint);
         }
 
 
@@ -102,12 +116,20 @@ public class CustomProgerssBar3 extends View {
         mPaint.setStyle(style==STROKE? Paint.Style.STROKE: Paint.Style.FILL);
         mPaint.setStrokeWidth(roundWidth);
         mPaint.setColor(roundColor);
-        RectF rectF = new RectF(roundWidth/2,roundWidth/2,getWidth()-roundWidth/2,getWidth()-roundWidth/2);
+        float left = centerX-radius;
+        float top = centerY-radius;
+        float right = centerX+radius;
+        float bottom = centerY+radius;
+        RectF rectF = new RectF(left,top,right,bottom);
         boolean useCenter = (style==FILL);
         LogUtil.i("(progress/(float)max*360)=   "+(progress/(float)max*360));
         canvas.drawArc(rectF,0,progress/(float)max*360,useCenter,mPaint);
     }
 
+    /**
+     * 自定义的属性progress,方便使用属性动画
+     * @param progress
+     */
     public synchronized void setProgress(int progress){
         this.progress = progress;
         invalidate();
