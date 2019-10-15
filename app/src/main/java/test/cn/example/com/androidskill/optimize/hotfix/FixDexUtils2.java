0abd4abfb36ebb569a994f21339e20b04630b34b
xugan <xugan@xywy.com>
2019-10-15 17:40:21 +0800

    1.在app这个module中调整了activity_hot_fix.xml文件 2.在HotFixActivity类中，新增fixBug2方法 3.新增了FixDexUtils2工具类

---
+++ app/src/main/java/test/cn/example/com/androidskill/optimize/hotfix/FixDexUtils2.java
@@ -0,0 +1,16 @@
+package test.cn.example.com.androidskill.optimize.hotfix;
+
+import java.lang.reflect.Field;
+
+public class FixDexUtils2 {
+    private FixDexUtils2(){}
+    public static Field getField(Class clazz,String fieldName) throws NoSuchFieldException {
+        return clazz.getDeclaredField(fieldName);
+    }
+
+    public static Object getObject(Class clazz,String fieldName,Object obj) throws NoSuchFieldException, IllegalAccessException {
+        Field field = getField(clazz, fieldName);
+        field.setAccessible(true);
+        return field.get(obj);
+    }
+}
--- app/src/main/java/test/cn/example/com/androidskill/optimize/hotfix/HotFixActivity.java
+++ app/src/main/java/test/cn/example/com/androidskill/optimize/hotfix/HotFixActivity.java
@@ -7,12 +7,24 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
+import dalvik.system.DexClassLoader;
+import dalvik.system.PathClassLoader;
 import test.cn.example.com.androidskill.R;
 import test.cn.example.com.util.LogUtil;
 import test.cn.example.com.util.ToastUtils;
@@ -28,6 +40,7 @@ public class HotFixActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_hot_fix);
         findViewById(R.id.btn_test).setOnClickListener(this);
         findViewById(R.id.btn_fix).setOnClickListener(this);
+        findViewById(R.id.btn_fix2).setOnClickListener(this);
     }
 
     @Override
@@ -40,6 +53,116 @@ public class HotFixActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.btn_fix:
                 fixBug();
                 break;
+            case R.id.btn_fix2:
+                try {
+                    fixBug2();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
+    private void fixBug2() throws FileNotFoundException, ClassNotFoundException {
+        File dir = getDir(MyConstant.DEX_DIR, Context.MODE_PRIVATE);
+        LogUtil.i(""+dir);
+        String name = "classes2.dex";
+        String filePath = dir.getAbsolutePath()+File.separator+name;
+        File file = new File(filePath);
+        if(file.exists()){
+            file.delete();
+        }
+        LogUtil.i(Environment.getExternalStorageDirectory().getAbsolutePath());
+        File fixFile = new File(Environment.getExternalStorageDirectory(),name);
+        if(null != fixFile){
+            LogUtil.i(""+fixFile.getAbsolutePath());
+            if(!fixFile.exists()){
+                throw new FileNotFoundException(name+"is not exists");
+            }
+        }
+
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(fixFile));
+        BufferedOutputStream bos = null;
+        try {
+            bos = new BufferedOutputStream(new FileOutputStream(file));
+            int len = 0;
+            byte[] buffer = new byte[1024];
+
+            while (-1!=(len = bis.read(buffer))){
+                bos.write(buffer,0,len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(null != bos){
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if(null != bis){
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if(file.exists()){
+            LogUtil.i("文件复制成功       "+file.getAbsolutePath());
+        }
+
+        //遍历app内部的存储了修复了bug的dex文件
+        File[] files = dir.listFiles();
+        File optDirFile = getDir("opt_dex", Context.MODE_PRIVATE);
+        if(optDirFile.exists()){
+            optDirFile.delete();
+        }
+        optDirFile.mkdirs();
+        LogUtil.i("修复的dex文件的解压路径    "+optDirFile.getAbsolutePath());
+        Class<?> baseDexClassLoaderClazz = Class.forName("dalvik.system.BaseDexClassLoader");
+        for(File f:files){
+            if(f.getName().startsWith("classes") && f.getName().endsWith(".dex")){
+                //2.获取DexClassLoader的实例，获取
+                try {
+                    DexClassLoader dexClassLoader = new DexClassLoader(f.getAbsolutePath(),optDirFile.getAbsolutePath(),"",getClassLoader());
+                    Object dexPathList = FixDexUtils2.getObject(baseDexClassLoaderClazz, "pathList",dexClassLoader);
+
+                    PathClassLoader pathClassLoader = (PathClassLoader) getClassLoader();
+                    Object pathList = FixDexUtils2.getObject(baseDexClassLoaderClazz, "pathList", pathClassLoader);
+
+                    Class<?> dexPathListClazz = Class.forName("dalvik.system.DexPathList");
+                    Object dexElements = FixDexUtils2.getObject(dexPathListClazz, "dexElements", dexPathList);
+                    Object pathDexElements = FixDexUtils2.getObject(dexPathListClazz, "dexElements", pathList);
+
+                    int i = Array.getLength(dexElements);
+                    int j = Array.getLength(pathDexElements);
+                    Object element = Array.get(pathDexElements, 0);
+                    Object newElements = Array.newInstance(element.getClass(), i + j);
+                    for(int k=0;k<(i+j);k++){
+                        if(k<i){
+                            Array.set(newElements,k,Array.get(dexElements,k));
+                        }else {
+                            Array.set(newElements,k,Array.get(pathDexElements,k-i));
+                        }
+                    }
+                    //合并完成后，将新的elements赋值给DexPathList的成员变量dexElements
+                    Field dexElementsField = FixDexUtils2.getField(dexPathListClazz, "dexElements");
+                    dexElementsField.setAccessible(true);
+                    dexElementsField.set(pathList,newElements);
+
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
