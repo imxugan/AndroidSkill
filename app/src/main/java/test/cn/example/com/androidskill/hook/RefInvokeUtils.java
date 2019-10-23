ba2076cb87feb45bbe862d4e4cc1559ddfa11e0d
xugan <xugan@xywy.com>
2019-10-23 08:33:04 +0800

    1.在app这个module中对RefInvokeUtils类，增加了一些方法

--- app/src/main/java/test/cn/example/com/androidskill/hook/RefInvokeUtils.java
+++ app/src/main/java/test/cn/example/com/androidskill/hook/RefInvokeUtils.java
@@ -8,6 +8,30 @@ public class RefInvokeUtils {
         return clazz.getDeclaredField(fieldName);
     }
 
+    public static Field getField(String className,String fieldName){
+        try {
+            Class<?> clazz = Class.forName(className);
+            Field declaredField = clazz.getDeclaredField(fieldName);
+            declaredField.setAccessible(true);
+            return declaredField;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Object getObject(String className,String fieldName,Object obj){
+        Field field = getField(className, fieldName);
+        try {
+            return field.get(obj);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public static Object getObject(Class clazz,String fieldName,Object obj) throws NoSuchFieldException, IllegalAccessException {
         Field field = getField(clazz, fieldName);
         field.setAccessible(true);
@@ -19,4 +43,14 @@ public class RefInvokeUtils {
         field.setAccessible(true);
         field.set(obj,value);
     }
+
+    public static void setObject(String className,String fieldName,Object obj,Object value){
+        Field field = getField(className, fieldName);
+        try {
+            field.set(obj,value);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
