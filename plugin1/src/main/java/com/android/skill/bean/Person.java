c95a614fe84e59625d3d74636da9c39f0ec95fea
xugan <xugan@xywy.com>
2019-10-23 10:14:58 +0800

    1.在项目中新增了一个plugin1这个application类型的module 2.将plugin1这个module生成的plugin1-debug.apk赋值到app这个module下的assets文件夹下

---
+++ plugin1/src/main/java/com/android/skill/bean/Person.java
@@ -0,0 +1,13 @@
+package com.android.skill.bean;
+
+public class Person {
+    private String name = "zma";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
