7d13530669addb5c3cacc3050a60b5395443a21e
xugan <xugan@xywy.com>
2019-10-24 14:06:49 +0800

    1.在plugin1这个module中，增加了将指定class文件打包到主dex中的演示

--- plugin1/src/main/java/com/android/skill/PluginOneApplication.java
+++ plugin1/src/main/java/com/android/skill/PluginOneApplication.java
@@ -1,10 +1,19 @@
 package com.android.skill;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 import android.util.Log;
 
 public class PluginOneApplication extends Application {
     private static final String TAG = "MY_LOG";
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
