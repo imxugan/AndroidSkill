434725113b86a74fe50924be6170874f85d2703f
xugan <xugan@xywy.com>
2019-11-18 15:24:14 +0800

    1.在app这个module中，新增了重新计算inSampleSize的功能

--- app/src/main/java/test/cn/example/com/androidskill/optimize/BitmapOptimizeActivity.java
+++ app/src/main/java/test/cn/example/com/androidskill/optimize/BitmapOptimizeActivity.java
@@ -16,6 +16,7 @@ public class BitmapOptimizeActivity extends AppCompatActivity implements View.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bitmap_optimize);
         findViewById(R.id.btn_1).setOnClickListener(this);
+        findViewById(R.id.btn_2).setOnClickListener(this);
 
 
     }
@@ -29,6 +30,33 @@ public class BitmapOptimizeActivity extends AppCompatActivity implements View.On
         LogUtil.i("width=   "+bitmap.getWidth()+"   height="+bitmap.getHeight()+"      byteCount=   "+byteCount);
     }
 
+    private void caculateBitmapSize(int inSampleSize){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher, options);
+        int byteCount = bitmap.getByteCount();
+        LogUtil.i("width=   "+bitmap.getWidth()+"   height="+bitmap.getHeight()+"   byteCount="+byteCount);
+    }
+
+    private int caculateInSampleSize(int reqWidth,int reqHeight){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher,options);
+        int width = options.outWidth;
+        int height = options.outHeight;
+        int inSampleSize = 1;
+        if(width>reqWidth || height>reqHeight){
+            int halfWidth = width/2;
+            int halfHeight = height/2;
+            while ((halfWidth/inSampleSize)>=reqWidth && (halfHeight/inSampleSize)>=reqHeight){
+                inSampleSize *=2;
+//            LogUtil.i("while    inSampleSize="+inSampleSize);
+            }
+        }
+        LogUtil.i("inSampleSize=    "+inSampleSize);
+        return inSampleSize;
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -37,6 +65,12 @@ public class BitmapOptimizeActivity extends AppCompatActivity implements View.On
                 caculteBitmapSize(Bitmap.Config.ARGB_4444);
                 caculteBitmapSize(Bitmap.Config.RGB_565);
                 caculteBitmapSize(Bitmap.Config.ALPHA_8);
+                break;
+            case R.id.btn_2:
+                caculateBitmapSize(caculateInSampleSize(100,100));
+                caculateBitmapSize(caculateInSampleSize(80,80));
+                caculateBitmapSize(caculateInSampleSize(50,50));
+                caculateBitmapSize(caculateInSampleSize(30,30));
                 break;
         }
     }
